7fe76a5f27d90e39836dd1b3dad0886466a242f6
xiaoj <showbufire@gmail.com>
2017-05-30 21:57:23 -0700

    [parser] simple ast_test

---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/showbufire/monkey/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myvar"},
+					Value: "myvar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myvar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
